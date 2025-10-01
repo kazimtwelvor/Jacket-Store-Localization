ded272c20c9f1807defbdd9ca8f5230b656fcd1e
Danzeeop234 <186355860+Danzeeop234@users.noreply.github.com>
2025-10-01 05:52:10 +0500

    feat: integrate Google Tag Manager for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5FDZTZJV');`
+        }} />
+        {/* End Google Tag Manager */}
+        
         {/* Google Site Verification */}
         <meta name="google-site-verification" content="7fzjFZWpRYLIz_L2sKypuvXyhyf44Na3by5X3a96l9g" />
         
@@ -67,6 +77,17 @@ export default function RootLayout({
         />
       </head>
       <body className={`${avertaDefault.variable} ${avertaBold.variable} w-full mx-0 px-0 bg-[#000000] min-h-screen flex flex-col`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5FDZTZJV"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <FontProvider>
           {/* <Suspense fallback={<div />}> */}
               <CartProvider>
